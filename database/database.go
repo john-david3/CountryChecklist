5fb7ab7e668ed0a6a979243846bf37206067e440
white <122345776@umail.ucc.ie>
2025-04-18 10:00:27 +0100

    Added some db operations

---
+++ database/database.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+func CreateConnection(){
+	var err error
+
+	db, err = sql.Open("sqlite3", "./app.db")
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+func Execute(query string, args ...interface{}){
+	if db == nil{
+		log.Fatal("DB needs to be initialised before executed on")
+	}
+
+	_, err := db.Exec(query, args...)
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+func Fetch(query string, args ...interface{}) *sql.Rows{
+	if db == nil{
+		log.Fatal("DB needs to be initialised before queried on")
+	}
+
+	rows, err := db.Query(query, args...)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	return rows
+}
+
+
+func CloseConnection(){
+	db.Close()
+}
---
+++ scripts/run_sqlite3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export CGO_ENABLED=1
+go run main.go
